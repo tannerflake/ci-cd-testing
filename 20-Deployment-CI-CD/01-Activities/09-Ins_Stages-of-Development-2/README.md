768f616eeb5f60bde602b661b2569c02cfdf439f
Tanner Flake <tannerflake@gmail.com>
2025-03-11 19:53:23 -0600

    added the word guessing game in the code

--- 20-Deployment-CI-CD/01-Activities/09-Ins_Stages-of-Development-2/README.md
+++
@@ -1,2 +0,0 @@
-# basic-production-ready
-This app is an example of a production-ready application using only Render.
--- 20-Deployment-CI-CD/01-Activities/28-Stu_Mini-Project/Develop/src/utils/interfaces/Film.interface.tsx
+++
@@ -1,9 +0,0 @@
-export default interface Film {
-  readonly Title: string | null;
-  readonly Director: string | null;
-  readonly Actors: string | null;
-  readonly Released: string | null;
-  readonly Poster: string | null;
-  readonly Genre: string | null;
-  readonly Plot: string | null;
-}
